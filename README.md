320c08822bab62cedada39258c81c806e893f773
adyanshkakran <adyanshkakran@gmail.com>
2023-04-18 17:45:41 +0530

    initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tarjan Vishkin Implementation
+
+This is a simple implementation of the Tarjan-Vishkin using Union Find  algorithm for finding the biconnected components of a graph. It is based on the algorithm in the [research paper](https://epubs.siam.org/doi/epdf/10.1137/0214061).
---
+++ random-graph.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+def generate_random_graph(num_vertices, num_edges):
+    G = nx.Graph()
+    # Add vertices
+    for i in range(num_vertices):
+        G.add_node(i)
+    # Add edges
+    while G.number_of_edges() < num_edges:
+        u = random.randint(0, num_vertices - 1)
+        v = random.randint(0, num_vertices - 1)
+        if u != v and not G.has_edge(u, v):
+            G.add_edge(u, v)
+    # Display the graph
+    print(G.number_of_nodes(), G.number_of_edges())
+    for edge in G.edges():
+        print(edge[0], edge[1])
+    nx.draw(G, with_labels=True)
+    plt.show()
+
+generate_random_graph(10, 20)
