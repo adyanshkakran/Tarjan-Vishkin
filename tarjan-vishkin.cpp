5f810bf594fe6b9f7a624bcd40e3c3ce7230a9d7
adyanshkakran <adyanshkakran@gmail.com>
2023-04-20 23:01:55 +0530

    auxillary graph in progress

--- tarjan-vishkin.cpp
+++ tarjan-vishkin.cpp
@@ -44,6 +44,15 @@ void bfs(graph* g, graph* t, graph* nt) {
     queue<vertex*> q;
     vector<bool> discovered(g->n, false), visited(g->n, false);
 
+    for(vertex* i : g->vertices) {
+        vertex* tt = new vertex();
+        tt->id = i->id;
+        t->vertices.push_back(tt);
+        vertex* ntt = new vertex();
+        ntt->id = i->id;
+        nt->vertices.push_back(ntt);
+    }
+
     for(int i = 0; i < g->n; i++){
         if(!visited[i]){
             q.push(g->vertices[i]);
@@ -52,7 +61,6 @@ void bfs(graph* g, graph* t, graph* nt) {
             discovered[g->vertices[i]->id] = true;
             while (!q.empty()) {
                 vertex* v = q.front();
-                t->vertices.push_back(v);
 
                 if(visited[v->id])
                     continue;
@@ -70,11 +78,17 @@ void bfs(graph* g, graph* t, graph* nt) {
                             u->parent = v;
                             v->children.push_back(u);
 
+                            edge* rev = reverseEdge(e);
                             t->edges.push_back(e);
-                            t->edges.push_back(reverseEdge(e));
+                            t->edges.push_back(rev);
+                            t->vertices[u->id]->edges.push_back(e);
+                            t->vertices[v->id]->edges.push_back(rev);
                         }else{
+                            edge* rev = reverseEdge(e);
                             nt->edges.push_back(e);
-                            nt->edges.push_back(reverseEdge(e));
+                            nt->edges.push_back(rev);
+                            nt->vertices[u->id]->edges.push_back(e);
+                            nt->vertices[v->id]->edges.push_back(rev);
                         }
                     }
                 }
@@ -117,8 +131,55 @@ void preOrderVertices(graph* t, vector<ll>& pre, vector<ll>& succ) {
     } while (i != 0);
 }
 
-void findLow(){
-    
+void findLow(graph* t, graph* nt, vector<ll>& low, vector<ll>& level){
+    low.resize(t->n);
+    for(ll i = 0; i < t->n; i++)
+        low[i] = i;
+
+    bool changed = true;
+    while(changed) {
+        changed = false;
+
+        for(ll i = 0; i < t->n; i++) {
+            for (edge* e : t->vertices[i]->edges) {
+                ll j = e->v1->id == i ? e->v2->id : e->v1->id;
+                if (level[i] < level[j] && level[low[j]] < level[low[i]]) {
+                    low[i] = low[j];
+                    changed = true;
+                }
+            }
+
+            for(edge* e : nt->vertices[i]->edges) {
+                ll j = e->v1->id == i ? e->v2->id : e->v1->id;
+                if (level[low[j]] < level[low[i]]) {
+                    low[i] = low[j];
+                    changed = true;
+                }
+            }
+        }
+    }
+}
+
+graph* auxillaryGraph(graph* g, graph* t, graph *nt) {
+    graph* aux = new graph();
+    aux->n = g->n + nt->m;
+    for(ll i = 0; i < aux->n; i++) {
+        vertex* v = new vertex();
+        v->id = i;
+        aux->vertices.push_back(v);
+    }
+    vector<ll> num(g->m,0);
+    ll nti = 0, ti = 0;
+    for(ll i = 0; i < g->m; i++){
+		if(nti >= nt->m || (g->edges[i]->v1 == t->edges[ti]->v1 && g->edges[i]->v2 == t->edges[ti]->v2))
+			ti++;
+		else
+		{
+			num[i] = 1;
+			nti++;
+		}
+	}
+    return aux;
 }
 
 int main() {
@@ -141,23 +202,37 @@ int main() {
         g->vertices[u]->edges.push_back(e);
         g->vertices[v]->edges.push_back(e);
         g->edges.push_back(e);
+        g->edges.push_back(reverseEdge(e));
     }
+    g->m *= 2;
     graph *t = new graph(), *nt = new graph();
     t->n = g->n;
+    nt->n = g->n;
     t->m = 2*(g->n-1);
+    nt->m = g->m - t->m;
     bfs(g,t,nt);
-    nt->n = nt->vertices.size();
-    nt->m = nt->edges.size();
     vector<ll> pre, succ, low;
+    sort(g->edges.begin(), g->edges.end(), cmp);
     sort(t->edges.begin(), t->edges.end(), cmp);
-    for (edge* e : t->edges)
-        cout << e->v1->id << " " << e->v2->id << endl;
+    sort(nt->edges.begin(), nt->edges.end(), cmp);
+    // for (edge* e : g->edges)
+    //     cout << e->v1->id << " " << e->v2->id << endl;
+    // cout << endl;
+    // for (edge* e : t->edges)
+    //     cout << e->v1->id << " " << e->v2->id << endl;
+    // cout << endl;
+    // for (edge* e : nt->edges)
+    //     cout << e->v1->id << " " << e->v2->id << endl;
     euler_tour(t, succ);
     // for (ll i : succ)
     //     cout << i << " ";
     // cout << endl;
     preOrderVertices(t, pre, succ);
     // for (ll i : pre)
+    //     cout << i << " ";
+    // cout << endl;
+    findLow(t, nt, low, pre);
+    // for (ll i : low)
     //     cout << i << " ";
     // cout << endl;
     
