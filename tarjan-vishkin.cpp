48611ae951852cabee0f17af4e23d5aad641a365
adyanshkakran <adyanshkakran@gmail.com>
2023-04-23 06:38:33 +0530

    tarjan-vishkin complete (tested as well)

--- tarjan-vishkin.cpp
+++ tarjan-vishkin.cpp
@@ -21,7 +21,6 @@ typedef struct Graph{
 
 edge* reverseEdge(edge* e){
     edge* rev = new edge();
-    // rev->id = id;
     rev->v1 = e->v2;
     rev->v2 = e->v1;
     return rev;
@@ -53,29 +52,37 @@ void bfs(graph* g, graph* t, graph* nt, vector<ll>& parent, vector<ll>& level) {
     queue<vertex*> q;
     vector<bool> discovered(g->n, false), visited(g->n, false);
 
+    q.push(g->vertices[0]);
+    discovered[0] = true;
+
     for(vertex* i : g->vertices) {
         vertex* tt = new vertex();
         tt->id = i->id;
         t->vertices.push_back(tt);
-        vertex* ntt = new vertex();
-        ntt->id = i->id;
-        nt->vertices.push_back(ntt);
+    }
+
+    for(vertex* i : g->vertices) {
+        vertex* tt = new vertex();
+        tt->id = i->id;
+        nt->vertices.push_back(tt);
     }
 
     for(int i = 0; i < g->n; i++){
         if(!visited[i]){
             q.push(g->vertices[i]);
+
             parent[g->vertices[i]->id] = g->vertices[i]->id;
             level[g->vertices[i]->id] = 0;
+                
             discovered[g->vertices[i]->id] = true;
             while (!q.empty()) {
                 vertex* v = q.front();
+                q.pop();
 
                 if(visited[v->id])
                     continue;
                 visited[v->id] = true;
 
-                q.pop();
                 for (edge* e : v->edges) {
                     vertex* u = e->v1 == v ? e->v2 : e->v1;
                     if (!visited[u->id]) {
@@ -97,6 +104,42 @@ void bfs(graph* g, graph* t, graph* nt, vector<ll>& parent, vector<ll>& level) {
                             nt->edges.push_back(rev);
                             nt->vertices[u->id]->edges.push_back(e);
                             nt->vertices[v->id]->edges.push_back(rev);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void findConnectedComponents(graph* g, vector<vector<ll>>& component) {
+    queue<vertex*> q;
+    vector<bool> discovered(g->n, false), visited(g->n, false);
+
+    q.push(g->vertices[0]);
+    discovered[0] = true;
+
+    for(int i = 0; i < g->n; i++){
+        if(!visited[i]){
+            q.push(g->vertices[i]);
+            discovered[g->vertices[i]->id] = true;
+
+            component.push_back(vector<ll>(1, g->vertices[i]->id));
+            while (!q.empty()) {
+                vertex* v = q.front();
+                q.pop();
+
+                if(visited[v->id])
+                    continue;
+                visited[v->id] = true;
+
+                for (edge* e : v->edges) {
+                    vertex* u = e->v1 == v ? e->v2 : e->v1;
+                    if (!visited[u->id]) {
+                        if (!discovered[u->id]) {
+                            discovered[u->id] = true;
+                            q.push(u);
+                            component.back().push_back(u->id);
                         }
                     }
                 }
@@ -140,17 +183,20 @@ void preOrderVertices(graph* t, vector<ll>& pre, vector<ll>& succ) {
 }
 
 void findLow(graph* t, graph* nt, vector<ll>& low, vector<ll>& level){
-    low.resize(t->n);
-    for(ll i = 0; i < t->n; i++)
+    low.resize(nt->n);
+
+    for(ll i = 0; i < nt->n; i++)
         low[i] = i;
 
     bool changed = true;
     while(changed) {
         changed = false;
 
-        for(ll i = 0; i < t->n; i++) {
+        for(ll i = 0; i < nt->n; i++) {
             for (edge* e : t->vertices[i]->edges) {
+
                 ll j = e->v1->id == i ? e->v2->id : e->v1->id;
+
                 if (level[i] < level[j] && level[low[j]] < level[low[i]]) {
                     low[i] = low[j];
                     changed = true;
@@ -158,7 +204,9 @@ void findLow(graph* t, graph* nt, vector<ll>& low, vector<ll>& level){
             }
 
             for(edge* e : nt->vertices[i]->edges) {
+
                 ll j = e->v1->id == i ? e->v2->id : e->v1->id;
+
                 if (level[low[j]] < level[low[i]]) {
                     low[i] = low[j];
                     changed = true;
@@ -179,7 +227,6 @@ graph* auxillaryGraph(graph* g, graph* t, graph *nt,vector<ll>& low, vector<ll>&
     vector<ll> num(g->m,0), pref(g->m,0);
     ll nti = 0, ti = 0;
     for(ll i = 0; i < g->m; i++){
-        cout << ti << " " << nti << endl;
 		if(nti >= nt->m || (ti < t->m && g->edges[i]->v1->id == t->edges[ti]->v1->id && g->edges[i]->v2->id == t->edges[ti]->v2->id))
 			ti++;
 		else
@@ -197,7 +244,7 @@ graph* auxillaryGraph(graph* g, graph* t, graph *nt,vector<ll>& low, vector<ll>&
     for(ll i = 0; i < g->m; i++) {
         ll u = g->edges[i]->v1->id, v = g->edges[i]->v2->id;
         if( nti >= nt->edges.size() || (ti < t->m && g->edges[i]->v1 == t->edges[ti]->v1 && g->edges[i]->v2 == t->edges[ti]->v2)){
-            if(level[u] < level[v] && level[low[u]] <= level[u]){
+            if(level[u] < level[v] && level[low[v]] <= level[u]){
                 edge* e = new edge();
                 e->v1 = aux->vertices[u];
                 e->v2 = aux->vertices[v];
@@ -230,6 +277,21 @@ graph* auxillaryGraph(graph* g, graph* t, graph *nt,vector<ll>& low, vector<ll>&
     return aux;
 }
 
+void remapAuxGraph(graph* t, vector<set<ll>>& bi, vector<vector<ll>>& components, vector<ll>& parent) {
+    bi.resize(components.size() - 1);
+	for(ll i = 1; i < components.size(); i++)
+	{
+		for(ll j = 0; j < components[i].size(); j++)
+		{
+			if(components[i][j] < t->n)
+			{
+				bi[i-1].insert(components[i][j]);
+				bi[i-1].insert(parent[components[i][j]]);
+			}
+		}
+	}
+}
+
 int main() {
     ifstream cin("datasets/graph1.txt");
     graph *g = new graph();
@@ -241,7 +303,6 @@ int main() {
     }
     for (int i = 0; i < g->m; i++) {
         edge *e = new edge();
-        // e->id = i*2;
         ll u, v;
         cin >> u >> v;
         e->v1 = g->vertices[u];
@@ -255,37 +316,42 @@ int main() {
     graph *t = new graph(), *nt = new graph();
     t->n = g->n;
     nt->n = g->n;
-    t->m = 2*(g->n-1);
-    nt->m = g->m - t->m;
     vector<ll> pre, succ, low, parent(g->n, -1), level(g->n, -1);
     bfs(g,t,nt, parent, level);
     sort(g->edges.begin(), g->edges.end(), cmp);
     sort(t->edges.begin(), t->edges.end(), cmp);
     sort(nt->edges.begin(), nt->edges.end(), cmp);
-    // for (edge* e : g->edges)
-    //     cout << e->v1->id << " " << e->v2->id << endl;
-    // cout << endl;
-    // for (edge* e : t->edges)
-    //     cout << e->v1->id << " " << e->v2->id << endl;
-    // cout << endl;
-    // for (edge* e : nt->edges)
-    //     cout << e->v1->id << " " << e->v2->id << endl;
+ 
+    t->m = t->edges.size();
+    nt->m = nt->edges.size();
+
     euler_tour(t, succ);
-    // for (ll i : succ)
-    //     cout << i << " ";
-    // cout << endl;
+
     preOrderVertices(t, pre, succ);
-    // for (ll i : pre)
-    //     cout << i << " ";
-    // cout << endl;
-    findLow(t, nt, low, pre);
-    // for (ll i : low)
-    //     cout << i << " ";
-    // cout << endl;
+ 
+    findLow(t, nt, low, level);
+
     graph* aux = auxillaryGraph(g, t, nt, low, level, parent, pre);
-    // print edges of auxillary graph
-    for (edge* e : aux->edges)
-        cout << e->v1->id << " " << e->v2->id << endl;
-    
+
+    vector<vector<ll>> connected;
+    findConnectedComponents(aux, connected);
+
+    vector<set<ll>> biconnected;
+    remapAuxGraph(t, biconnected, connected, parent);
+
+    for(ll i = 0; i < biconnected.size(); i++) {
+        if(biconnected[i].size() == 0){
+            biconnected.erase(biconnected.begin() + i);
+            i--;
+        }
+    }
+    // cout << t->n << " " << t->m << endl;
+    // cout << nt->n << " " << nt->m << endl;
+    // cout << aux->n << " " << aux->m << endl;
+    for (set<ll> s : biconnected) {
+        for (ll i : s)
+            cout << i << " ";
+        cout << endl;
+    }
     return 0;
 }
